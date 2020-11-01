a989c5ef9ed8606f173df9e2025910a8e312420e
abasiyanik <abasiyanik@gmail.com>
2020-11-01 09:40:20 -0600

    forth

--- src/main/java/november1.java
+++
@@ -1,5 +0,0 @@
-public class november1 {
-    public static void main(String[] args) {
-        System.out.println("november");
-    }
-}
--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -1,4 +1,4 @@
-public class november {
+public class Batman {
     public static void main(String[] args) {
         System.out.println("november");
     }
